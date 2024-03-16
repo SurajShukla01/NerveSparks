61825ebd402e7f833d6934219a93ae679440ea04
Suraj Shukla <shukla.suraj0101@gmail.com>
2024-03-17 03:23:04 +0530

    added fix to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,6 +22,12 @@ app.get('/', (req, res) => {
     res.send("Welcome")
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use('/api/admin', adminRoutes)
 app.use('/api/user', userRouter)
 app.use('/api/dealer', dealerRouter)
