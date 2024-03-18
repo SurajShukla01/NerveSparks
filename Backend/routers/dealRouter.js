ba752c9dbd91d07cba411a2cc6db806df118d81c
Suraj Shukla <shukla.suraj0101@gmail.com>
2024-03-18 21:43:20 +0530

    fixed dealer get all

--- Backend/routers/dealRouter.js
+++ Backend/routers/dealRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { createDeal, getAllDeals } = require('../controller/dealController')
 const { handleAsync } = require('../utils/errorHandler')
-const { velidateDealer, protect } = require('../middleware/authMiddleware')
+const { velidateDealer, protect, velidateUserDealer } = require('../middleware/authMiddleware')
 
 const router = express.Router()
 router.post('/create/:dID',handleAsync(protect) ,velidateDealer, handleAsync(createDeal))
